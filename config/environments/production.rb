fce84f4f92e8d8cc1227d546a859bc34f06e9cd8
chapelle <ukya.yamanashi@gmail.com>
2020-03-24 12:02:41 +0900

    Set SendGrid user name and password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,8 @@ Rails.application.configure do
     :adresss => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    # :user_name => ENV[''],
-    # :password => ENV[''],
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
